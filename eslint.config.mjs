564f155bd85f13841df1d3cb5b3efe60ebfdb434
Michał Konopski <michal.konopski@camunda.com>
2025-05-19 21:04:00 +0200

    chore: update config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,6 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import tsParser from '@typescript-eslint/parser';
 import testingLibrary from 'eslint-plugin-testing-library';
 import prettier from 'eslint-plugin-prettier';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import type { Linter, ESLint } from 'eslint';
 import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 
@@ -31,7 +29,7 @@ export default defineConfig([
     plugins: {
       'testing-library': testingLibrary,
       prettier,
-      '@typescript-eslint': typescriptEslint as unknown as ESLint.Plugin,
+      '@typescript-eslint': tseslint,
     },
 
     rules: {
@@ -64,4 +62,4 @@ export default defineConfig([
     '.swc',
     'node_modules',
   ]),
-] satisfies Linter.Config[]);
+]);
